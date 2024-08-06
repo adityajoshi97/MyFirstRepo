9235f6bf38ce0dc0a5de04c88290b441f699aa47
aditya <adityajoshi389@gmail.com>
2024-08-07 00:39:25 +0530

    Added num variable

--- Hello.java
+++ Hello.java
@@ -1,6 +1,15 @@
 public class Hello {
+
+    private int num = 10;
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        Hello hello = new Hello();
+        hello.showNum();
+    }
+
+    public void showNum() {
+        System.out.println("Number : " + num);
     }
 
 }
